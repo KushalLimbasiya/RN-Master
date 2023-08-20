d064a722ca2e4f20d0927b803577aeb3e3767491
kushal limbasiya <kushallimbasiya@gmail.com>
2023-08-20 23:51:28 +0530

    add linear gradient

--- src/screens/day005/HomeScreen.tsx
+++ src/screens/day005/HomeScreen.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from "@react-navigation/native"
+import LinearGradient from 'react-native-linear-gradient';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
     <View className="flex-1 bg-bg">
+      <LinearGradient colors={['rgba(157, 67, 199, 0.00)', 'rgba(199, 79, 236, 0.07)']} style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        
       <SafeAreaView className='flex'>
         <View className='flex-row justify-start'>
           <TouchableOpacity
@@ -61,6 +64,7 @@ const HomeScreen = () => {
         </View>
 
       </View>
+      </LinearGradient>
     </View>
     
   )
